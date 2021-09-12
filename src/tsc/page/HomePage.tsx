13f5e790c420b730c133d53e4597256bd4ad359c
Your Name <you@example.com>
2021-09-11 20:39:09 -0500

    cambios estilos

--- src/tsc/page/HomePage.tsx
+++ src/tsc/page/HomePage.tsx
@@ -6,30 +6,47 @@ import * as common from "../constant/common";
 import CategoryBox from "../component/nav/CategoryBox";
 import FFC from  '../../../assets/img/ffc.png';
 
-export default class HomePage extends Component {
+export default class HomePage extends Component<{}, { innerWidth: string}> {
     constructor(props: any){
         super(props);
+        this.state = {
+            innerWidth: window.innerHeight + "px"
+        };
     }
 
-    public get WinHeight() {
-        return window.innerHeight + "px";
+    private resizeEvent: any;
+
+    componentDidMount(){
+        let me = this;
+        me.resizeEvent = function(){
+            let innerWidth = window.innerHeight + "px";
+            me.setState({
+                innerWidth 
+            });
+        }
+        window.onresize = me.resizeEvent;
+    }
+    
+    componentWillUnmount(){
+        let me = this;
+        window.removeEventListener("resize", me.resizeEvent);
     }
 
     render(){
         let me = this;
         return(
             <div className="wrapper">
-                <div className="home" style={{height: me.WinHeight }}>
+                <div className="home" style={{height: me.state.innerWidth }}>
                     <div className="home-bar-image">
                         <div className="home-cell">
                             <h3>JPIC</h3>
                             <div className="image image__jpic"></div>
                         </div>
-                        <div className="home-cell image__fco_jesus">
+                        <div className="home-cell">
                             <h3>Formación</h3>
                             <div className="image image__fco_jesus"></div>
                         </div>
-                        <div className="home-cell image__donate">
+                        <div className="home-cell">
                             <h3>Donaciones</h3>
                             <div className="image image__donate"></div>
                         </div>
@@ -48,15 +65,15 @@ export default class HomePage extends Component {
                         </div>
                     </div>
                     <div className="home-bar-image">
-                        <div className="home-cell image__fco_rendon">
+                        <div className="home-cell">
                             <h3>Oratorio</h3>
                             <div className="image image__fco_rendon"></div>
                         </div>
-                        <div className="home-cell image__jufra">
+                        <div className="home-cell">
                             <h3>Jóvenes</h3>
                             <div className="image image__jufra"></div>
                         </div>
-                        <div className="home-cell image__tejido_artesanal">
+                        <div className="home-cell">
                             <h3>Tienda</h3>
                             <div className="image image__tejido_artesanal"></div>
                         </div>
